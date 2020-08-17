5a01a4361f10ee34638be323ff7470c804336821
omsatam <62635289+omsatam@users.noreply.github.com>
2020-08-17 19:12:20 +0530

    uploaded chat app without node modules

---
+++ chat app/main.js
@@ -0,0 +1,64 @@
+const socket = io('http://localhost:8000');
+let name;
+let message = document.getElementById('message-input')
+let messagearea = document.getElementById('message-area')
+let button = document.getElementById("button")
+let container = document.getElementById("container")
+let incoming = new Audio('img/incoming.mp3')
+let outgoing = new Audio('img/outgoing.mp3')
+
+do{
+    name = prompt('Please enter your name: ')
+} while (!name)
+socket.emit('new-user-joined',name);
+
+button.addEventListener('click',() =>{
+    if (message.value != ""){
+    console.log(message.value)
+    sendMessage(message.value)
+    outgoing.play();
+    message.value = "";
+    }
+})
+
+function sendMessage(message) {
+    // Append 
+    socket.emit('send', message) 
+    appendMessage(`You: ${message}`, 'right')
+
+}
+
+function appendMessage(msg, type) {
+ let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className,'message-text','message')
+    
+    let markup = `
+            ${msg}
+        `
+    mainDiv.innerHTML = markup
+    messagearea.appendChild(mainDiv)
+    scrollToBottom()
+
+}
+
+// Recieve messages 
+// socket.on('message', (msg) => {
+//     appendMessage(msg, 'left')
+//     scrollToBottom()
+// })
+socket.on('user-joined',name =>{
+   appendMessage(`${name} joined the chat`,'center')
+    })
+    
+ socket.on('recieve',data =>{
+        appendMessage(`${data.name}: ${data.message}`,'left')
+        incoming.play()
+    })
+    
+ socket.on('left',name =>{
+        appendMessage(`${name} left the chat`,'center')
+    })
+function scrollToBottom(){
+    container.scrollTop = container.scrollHeight
+}
---
+++ chat app/nodeserver/index.js
@@ -0,0 +1,21 @@
+// node server which will handle socket io connections
+const io = require('socket.io')(8000)
+const users = {};
+io.on('connection', socket => {
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        console.log(`user joined ${users[socket.id]}`)
+   
+    socket.broadcast.emit('user-joined', name);    
+    });
+
+
+socket.on('send',message =>{ 
+    socket.broadcast.emit('recieve',{name: users[socket.id], message:message})
+});
+
+socket.on('disconnect', message =>{
+    socket.broadcast.emit('left', users[socket.id]); 
+    delete users[socket.id];
+});
+})
