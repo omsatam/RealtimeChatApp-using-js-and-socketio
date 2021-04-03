67c530a26baf119c22eb2a55f53762445da19fe5
omsatam <62635289+omsatam@users.noreply.github.com>
2021-04-03 12:34:04 +0530

    Update main.js

--- chat app/main.js
+++ chat app/main.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://realtime-chat-app-using-js-and-socketio-de7o775a3-omsatam.vercel.app/');
 let name;
 let message = document.getElementById('message-input')
 let messagearea = document.getElementById('message-area')
