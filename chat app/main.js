416d763b1e8db3f17c06e182c898a0ee58b381fc
omsatam <62635289+omsatam@users.noreply.github.com>
2021-04-22 19:17:23 +0530

    Update main.js

--- chat app/main.js
+++ chat app/main.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-app-socketioserver.herokuapp.com/');
+const socket = io('https://chat-app-socketioserver.herokuapp.com');
 let name;
 let message = document.getElementById('message-input')
 let messagearea = document.getElementById('message-area')
